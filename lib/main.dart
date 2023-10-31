ee0a50d094fe7256e5afaffe84ced2bb37ee0ddd
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-31 13:58:54 +0200

    3. Initialize the Firebase depending on the Current Platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
-import 'package:chat_app/constants/strings.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'constants/strings.dart';
+import 'firebase_options.dart';
 import 'screens/sign_in_screen.dart';
 import 'screens/sign_up_screen.dart';
 
-void main() => runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const App());
+}
 
 class App extends StatelessWidget {
   const App({super.key});
