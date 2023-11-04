cfcb39e677e69c06425060a1ff79f9c9155f436a
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-04 12:56:38 +0200

    3. Build MessageTextField Widget to Send Messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -9,25 +9,48 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
         title: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Image.asset(
-              kLogo,
-              height: 48,
-            ),
+            Image.asset(kLogo, height: 48),
             const Text('Chat'),
           ],
         ),
       ),
-      body: ListView.builder(
-        padding: const EdgeInsetsDirectional.all(16),
-        itemCount: 10,
-        itemBuilder: (context, index) => const ChatBubble(
-          text: 'I am excellent in my work',
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsetsDirectional.all(16),
+              itemCount: 10,
+              itemBuilder: (context, index) => const ChatBubble(
+                text: 'I am excellent in my work',
+              ),
+            ),
+          ),
+          const MessageTextField(),
+        ],
+      ),
+    );
+  }
+}
+
+class MessageTextField extends StatelessWidget {
+  const MessageTextField({super.key});
+
+  @override
+  Widget build(context) {
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Send a message',
+          suffixIcon: Icon(Icons.send_rounded),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
         ),
       ),
     );
