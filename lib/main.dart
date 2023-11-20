a7ec3cee9d8bcf408d468224ea9331fc4d9e27ca
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-20 23:52:36 +0200

    6. Provide AuthCubit above the AuthScreen Only

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'constants/strings.dart';
-import 'cubits/auth/auth_cubit.dart';
 import 'firebase_options.dart';
 import 'screens/auth_screen.dart';
 import 'screens/chat_screen.dart';
@@ -20,19 +18,16 @@ class App extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return BlocProvider(
-      create: (context) => AuthCubit(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          brightness: Brightness.dark,
-        ),
-        routes: {
-          kAuthScreen: (context) => const AuthScreen(),
-          kChatScreen: (context) => ChatScreen(),
-        },
-        initialRoute: kAuthScreen,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
       ),
+      routes: {
+        kAuthScreen: (context) => const AuthScreen(),
+        kChatScreen: (context) => ChatScreen(),
+      },
+      initialRoute: kAuthScreen,
     );
   }
 }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart'
-    show BlocConsumer, BlocSelector, ReadContext;
+    show BlocConsumer, BlocProvider, BlocSelector, ReadContext;
 
 import '../constants/strings.dart';
 import '../cubits/auth/auth_cubit.dart';
@@ -28,83 +28,86 @@ class _AuthScreenState extends State<AuthScreen> {
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Form(
           key: _formKey,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 80),
-                child: Column(
-                  children: [
-                    Image.asset(kLogo),
-                    const Text(
-                      kAppName,
-                      style: TextStyle(fontSize: 32, fontFamily: 'Pacifico'),
-                    ),
-                  ],
-                ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: BlocSelector<AuthCubit, AuthState, bool>(
-                  selector: (state) => state.isSignIn,
-                  builder: (context, isSignIn) => Text(
-                    isSignIn ? kSignInLabel : kSignUpLabel,
-                    style: const TextStyle(fontSize: 24),
+          child: BlocProvider(
+            create: (context) => AuthCubit(),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 80),
+                  child: Column(
+                    children: [
+                      Image.asset(kLogo),
+                      const Text(
+                        kAppName,
+                        style: TextStyle(fontSize: 32, fontFamily: 'Pacifico'),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              const SizedBox(height: 24),
-              MainTextFormField(
-                label: kEmailLabel,
-                keyboardType: TextInputType.emailAddress,
-                onSaved: (value) => email = value ?? '',
-              ),
-              const SizedBox(height: 16),
-              MainTextFormField(
-                label: kPasswordLabel,
-                obscureText: true,
-                onSaved: (value) => password = value ?? '',
-              ),
-              const SizedBox(height: 24),
-              BlocConsumer<AuthCubit, AuthState>(
-                listener: (context, state) {
-                  if (state.authStatus == AuthStatus.success) {
-                    Navigator.pushReplacementNamed(
-                      context,
-                      kChatScreen,
-                      arguments: state.userEmail,
-                    );
-                  } else if (state.authStatus == AuthStatus.failure) {
-                    showSnackBar(context, state.message);
-                  }
-                },
-                builder: (context, state) => MainElevatedButton(
-                  label: state.isSignIn ? kSignInLabel : kSignUpLabel,
-                  isLoading: state.authStatus == AuthStatus.loading,
-                  onPressed: () {
-                    if (isFormValid) {
-                      context.read<AuthCubit>().authenticate(
-                            email: email,
-                            password: password,
-                          );
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: BlocSelector<AuthCubit, AuthState, bool>(
+                    selector: (state) => state.isSignIn,
+                    builder: (context, isSignIn) => Text(
+                      isSignIn ? kSignInLabel : kSignUpLabel,
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                MainTextFormField(
+                  label: kEmailLabel,
+                  keyboardType: TextInputType.emailAddress,
+                  onSaved: (value) => email = value ?? '',
+                ),
+                const SizedBox(height: 16),
+                MainTextFormField(
+                  label: kPasswordLabel,
+                  obscureText: true,
+                  onSaved: (value) => password = value ?? '',
+                ),
+                const SizedBox(height: 24),
+                BlocConsumer<AuthCubit, AuthState>(
+                  listener: (context, state) {
+                    if (state.authStatus == AuthStatus.success) {
+                      Navigator.pushReplacementNamed(
+                        context,
+                        kChatScreen,
+                        arguments: state.userEmail,
+                      );
+                    } else if (state.authStatus == AuthStatus.failure) {
+                      showSnackBar(context, state.message);
                     }
                   },
+                  builder: (context, state) => MainElevatedButton(
+                    label: state.isSignIn ? kSignInLabel : kSignUpLabel,
+                    isLoading: state.authStatus == AuthStatus.loading,
+                    onPressed: () {
+                      if (isFormValid) {
+                        context.read<AuthCubit>().authenticate(
+                              email: email,
+                              password: password,
+                            );
+                      }
+                    },
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8),
-              BlocSelector<AuthCubit, AuthState, bool>(
-                selector: (state) => state.isSignIn,
-                builder: (context, isSignIn) => Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(isSignIn ? kSignUpMessage : kSignInMessage),
-                    TextButton(
-                      onPressed: context.read<AuthCubit>().toggleAuth,
-                      child: Text(isSignIn ? kSignUpLabel : kSignInLabel),
-                    ),
-                  ],
+                const SizedBox(height: 8),
+                BlocSelector<AuthCubit, AuthState, bool>(
+                  selector: (state) => state.isSignIn,
+                  builder: (context, isSignIn) => Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(isSignIn ? kSignUpMessage : kSignInMessage),
+                      TextButton(
+                        onPressed: context.read<AuthCubit>().toggleAuth,
+                        child: Text(isSignIn ? kSignUpLabel : kSignInLabel),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
