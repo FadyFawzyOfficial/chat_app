3d713940cab6687dc0f90d910296734532331e42
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-20 19:56:44 +0200

    3. Build Auth Cubit Methods (authenticate() method)

--- lib/cubits/auth/auth_cubit.dart
+++ lib/cubits/auth/auth_cubit.dart
@@ -1,7 +1,40 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart'
+    show FirebaseAuth, FirebaseAuthException;
+import 'package:flutter_bloc/flutter_bloc.dart' show Cubit;
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthState.initial());
+
+  void authenticate({
+    required bool isSigningIn,
+    required String email,
+    required String password,
+  }) async {
+    emit(state.copyWith(authStatus: AuthStatus.loading));
+
+    try {
+      final userCredential = isSigningIn
+          ? await FirebaseAuth.instance.signInWithEmailAndPassword(
+              email: email,
+              password: password,
+            )
+          : await FirebaseAuth.instance.createUserWithEmailAndPassword(
+              email: email,
+              password: password,
+            );
+
+      emit(
+        state.copyWith(
+          authStatus: AuthStatus.success,
+          userEmail: userCredential.user!.email,
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      emit(state.copyWith(authStatus: AuthStatus.failure, message: e.message));
+    } catch (e) {
+      emit(state.copyWith(authStatus: AuthStatus.failure, message: '$e'));
+    }
+  }
 }
