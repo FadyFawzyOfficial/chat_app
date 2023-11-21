1724f8fc3c45693c52c93303ceed316e1f68bba2
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-21 10:19:49 +0200

    3. Build AuthBloc itself to Receive Events & Emit States

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'auth_event.dart';
@@ -5,6 +6,37 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthState.initial()) {
-    on<AuthEvent>((event, emit) {});
+    on<AuthToggled>(
+        (event, emit) => emit(state.copyWith(isSignIn: !state.isSignIn)));
+
+    on<AuthAuthenticated>(
+      (event, emit) async {
+        emit(state.copyWith(authStatus: AuthStatus.loading));
+
+        try {
+          final userCredential = state.isSignIn
+              ? await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: event.email,
+                  password: event.password,
+                )
+              : await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: event.email,
+                  password: event.password,
+                );
+
+          emit(
+            state.copyWith(
+              authStatus: AuthStatus.success,
+              userEmail: userCredential.user!.email,
+            ),
+          );
+        } on FirebaseAuthException catch (e) {
+          emit(state.copyWith(
+              authStatus: AuthStatus.failure, message: e.message));
+        } catch (e) {
+          emit(state.copyWith(authStatus: AuthStatus.failure, message: '$e'));
+        }
+      },
+    );
   }
 }
