57d8544b8f131c03d589fe6503d1558b4c84d3c7
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-21 10:49:47 +0200

    2. Set the BlocObserver of the App to be the Created SimpleBlocObserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart' show Bloc;
 
 import 'constants/strings.dart';
 import 'firebase_options.dart';
 import 'screens/auth_screen.dart';
 import 'screens/chat_screen.dart';
+import 'simple_bloc_observer.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
