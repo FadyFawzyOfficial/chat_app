a02499f4119abc8a022ba92549f30efd1fedf617
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-11-21 10:19:17 +0200

    2. Create & Build AuthBloc Events

--- lib/blocs/auth/auth_event.dart
+++ lib/blocs/auth/auth_event.dart
@@ -1,3 +1,15 @@
 part of 'auth_bloc.dart';
 
-sealed class AuthEvent {}
+abstract class AuthEvent {}
+
+class AuthAuthenticated extends AuthEvent {
+  final String email;
+  final String password;
+
+  AuthAuthenticated({
+    required this.email,
+    required this.password,
+  });
+}
+
+class AuthToggled extends AuthEvent {}
